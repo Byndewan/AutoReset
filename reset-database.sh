417f2564dfbd3ef1d51e35b5e1c7a2489d11f626
byndewan <abyandev657@gmail.com>
2025-02-19 09:37:00 +0700

    Second

---
+++ reset-database.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Masuk ke folder project Laravel
+cd /C:/xampp/htdocs/Progress_Web_RD
+
+# Hentikan jika ada error
+set -e
+
+# Reset database
+php artisan migrate:refresh --seed
+
+# Opsional: Hapus cache dan build ulang
+php artisan cache:clear
+php artisan config:clear
+php artisan view:clear
+
+echo "Data Berhasil di-reset!"
